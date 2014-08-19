810835ea04bdf7f08aae719a6c0bd3b7d9da296c
tianze.cui <tianze.cui@baidao.com>
2014-08-19 14:23:12 +0800

    新版翻页欢迎页

---
+++ src/app/templates.js
@@ -0,0 +1,13 @@
+define(function(require, exports, module) {
+
+	module.exports = {
+		'mountain': '<section><div class="full-image" style="background-image: url(img/img1.jpg); background-size: cover"></div></section>',
+		'trail': '<section><div class="full-image" style="background-image: url(img/img2.jpg); "></div></section>',
+		'concert': '<section><div class="full-image" style="background-image: url(img/img3.jpg); "></div></section>',
+		'sunset': '<section><div class="full-image" style="background-image: url(img/img4.jpg); "></div></section>',
+		'clouds': '<section><div class="full-image" style="background-image: url(img/img5.jpg); "></div></section>',
+		// 'story': '<section><div class="full-image" style="background-color: #fff; "></div><div class="overlay-content light-bg"><h2>Everyone</h2><h3>has a story to tell</h3></div></section>',
+		// 'field': '<section><div class="full-image" style="background-image: url(img/field.jpg); "></div><div class="overlay-content normal-font"><h3>Tell yours on steller</h3></div></section>'
+	}
+
+});
